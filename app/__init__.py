06d7b4340c36dcf2e4bd9895a602702eaf6e8d6e
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-30 05:15:43 +0100

    running

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Initialize the SodLat Edu Solution application.
 """
@@ -7,8 +7,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from app.models import User
+
 
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
 
 def create_app():
@@ -16,7 +19,9 @@ def create_app():
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     from app.routes.main import main
     from app.routes.auth import auth
@@ -31,3 +36,10 @@ def create_app():
     app.register_blueprint(parent)
 
     return app
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
